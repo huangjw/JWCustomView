ade12af0874bad942761ddaa60bd0f6cfb333f60
huangjw <jiawei.huang@midea.com>
2017-01-07 11:29:13 +0800

    First commit

---
+++ JWCustomView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JWCustomView
+//
+//  Created by huangjw on 2017/1/7.
+//  Copyright © 2017年 huangjw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JWCustomView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JWCustomView
+//
+//  Created by huangjw on 2017/1/7.
+//  Copyright © 2017年 huangjw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
